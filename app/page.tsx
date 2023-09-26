e2e5085ee94972dfb1c854428b068af94a333328
Natalia Orsi <naty.m.orsi@gmail.com>
2023-09-26 11:00:22 +0200

    Added the link to the game

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="my-44">
-      <div className="box-border rounded-xl max-w-7xl h-[32rem] border-yellow-50/80 border-2 m-auto bg-yellow-50/80 flex justify-center items-center relative">
+      <div className="justify-center items-center">
         <div className="animated-text text-8xl text-center">
           <span className="text-primary-color">H</span>
           <span className="text-secondary-color">A</span>
@@ -11,8 +13,10 @@ export default function Home() {
           <span className="text-third-color">A</span>
           <span className="text-primary-color">N</span>
         </div>
-        <div className="border-third-color border-2 bg-third-color text-white rounded-md absolute self-end m-4">
-          <button className="p-4">Let's play</button>
+        <div className="border-third-color border-2 bg-third-color text-white rounded-md flex absolute left-[47%] bottom-[26%]">
+          <Link href='/game'>
+            <button className="p-4">Let's play</button>
+          </Link>
         </div>
       </div>
     </main>
