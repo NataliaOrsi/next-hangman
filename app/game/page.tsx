fb8c86e1049767f128ffa567b3327e693713aacf
Natalia Orsi <naty.m.orsi@gmail.com>
2023-09-28 11:23:27 +0200

    Updated the messages options

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { FormEvent, JSXElementConstructor, useState } from "react";
+import React, { FormEvent, JSXElementConstructor, use, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark, faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
@@ -8,85 +8,93 @@ import Link from "next/link";
 export default function game() {
   const [guess, setGuess] = useState("");
   const [lettersGuessed, setLettersGuessed] = useState<string[]>([]);
-
-  const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    .split("")
-    .join(" ")
-    .toUpperCase();
+  const [alphabet, setAlphabet] = useState(
+    "abcdefghijklmnopqrstuvwxyz".split("")
+  );
+    
   const numberOfGuesses = 8;
   const secretWord = "apple".split("");
-  
 
 
-  const isWordGuessed = (secretWord: string[], lettersGuessed: string[]) => {
-    const word = [];
+  // const isWordGuessed = (secretWord: string[], lettersGuessed: string[]) => {
+  //   const word = [];
 
-    for (const letter in secretWord) {
-      if (letter in lettersGuessed) {
-        word.push(letter);
-      }
+  //   for (const letter in secretWord) {
+  //     if (letter in lettersGuessed) {
+  //       word.push(letter);
+  //     }
 
-      return word == secretWord;
-    }
-  };
+  //     return word == secretWord;
+  //   }
+  // };
 
-  const getGuessedWord = (secretWord: string[], lettersGuessed: string[]) => {
-    const symbol = "_ ";
-    const word_to_fill = [];
-    for (const letter in secretWord) {
-      if (letter in lettersGuessed) {
-        word_to_fill.push(letter);
-      } else {
-        word_to_fill.push(letter);
-      }
-      return word_to_fill;
-    }
-  };
+  // const getGuessedWord = (secretWord: string[], lettersGuessed: string[]) => {
+  //   const symbol = "_ ";
+  //   const word_to_fill = [];
+  //   for (const letter in secretWord) {
+  //     if (letter in lettersGuessed) {
+  //       word_to_fill.push(letter);
+  //     } else {
+  //       word_to_fill.push(letter);
+  //     }
+  //     return word_to_fill;
+  //   }
+  // };
 
-  
-  isWordGuessed(secretWord, ["A"]);
+
 
   function handleSubmit(event: FormEvent<HTMLFormElement>): void {
     event.preventDefault();
     setLettersGuessed((e) => [...e, guess.toLowerCase()]);
-    setGuess('');
+    setAlphabet(alphabet.filter((i) => i != guess.toLowerCase()));
+    setGuess("");
   }
 
-  const isLetterIn = (): boolean => {
-    return secretWord.indexOf(guess.toLowerCase()) > -1;
-  }
 
+  const isLetterIn = secretWord.indexOf(guess.toLowerCase()) > -1
+  const isLetterAvailable = !(lettersGuessed.indexOf(guess.toLowerCase()) > -1);
+
+  const Greeting =
+    guess === ""
+      ? ""
+      : isLetterAvailable
+      ? isLetterIn
+        ? "Good guess!"
+        : "Oops! That letter is not in my word!"
+      : "Oops! You've already guessed that letter";
+
+  
   return (
     <div>
-      <Link href="/">
-        <FontAwesomeIcon
-          icon={faCircleXmark}
-          beat
-          size="lg"
-          style={{ color: "#53cdd8" }}
-          className="mt-2 ml-2"
-        />
-      </Link>
+      <form onSubmit={handleSubmit}>
+        <Link href="/">
+          <FontAwesomeIcon
+            icon={faCircleXmark}
+            beat
+            size="lg"
+            style={{ color: "#53cdd8" }}
+            className="mt-2 ml-2"
+          />
+        </Link>
 
-      <p className="text-2xl text-third-color text-center">
-        Available letters: {alphabet}
-      </p>
-      <div className="flex justify-center self-center mt-40">
-        <div className="box-border h-20 w-20 p-4 border-2 border-white bg-white">
-          <p className="text-4xl text-primary-color text-center">A</p>
+        <p className="text-2xl text-third-color text-center">
+          Available letters: {alphabet.join(" ").toUpperCase()}
+        </p>
+        <div className="flex justify-center self-center mt-40">
+          <div className="box-border h-20 w-20 p-4 border-2 border-white bg-white">
+            <p className="text-4xl text-primary-color text-center">A</p>
+          </div>
         </div>
-      </div>
-      <p className="text-primary-color text-2xl flex justify-center self-center mt-20">
-        You have {numberOfGuesses} guesses left
-      </p>
-      <div className="flex flex-col justify-center mt-4">
-        <label className="text-secondary-color font-bold text-xl relative left-[45%]">
-          Guess a letter:{" "}
-        </label>
-        <div>
-          <form onSubmit={handleSubmit}>
+        <p className="text-primary-color text-2xl flex justify-center self-center mt-20">
+          You have {numberOfGuesses} guesses left
+        </p>
+        <div className="flex flex-col justify-center mt-4">
+          <label className="text-secondary-color font-bold text-xl relative left-[44%]">
+            Guess a letter:{" "}
+          </label>
+          <div>
             <input
-              className="p-2 rounded-md relative left-[41.3%] text-center"
+              className="p-2 rounded-md relative left-[40%] text-center"
               type="text"
               value={guess}
               onChange={(e) => {
@@ -102,18 +110,12 @@ export default function game() {
                 style={{ color: "#53cdd8" }}
               />
             </button>
-          </form>
+          </div>
         </div>
-      </div>
-      {isLetterIn() ? (
         <p className="text-third-color font-bold text-3xl text-center m-8">
-          Good guess!
+          {Greeting}
         </p>
-      ) : (
-        <p className="text-third-color font-bold text-3xl text-center m-8">
-          Oops! That letter is not in my word!
-        </p>
-      )}
+      </form>
     </div>
   );
 }
